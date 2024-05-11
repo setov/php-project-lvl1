de125df50e9888b5e6f71a1d7e87ebc4e6413cb0
Sergey Titov <ya.sergeTitov@yandex.ru>
2024-05-11 18:19:37 +0300

    Add wp-cli library
    Add wp-cli library to project. Add src/Cli.php file using this
    library.
    Rewrite bin/brain-games file using function greeting()
    from src/Cli.php.

---
+++ src/Cli.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brain\Games\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function greeting(): void
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name? ', 'User');
+    line("Hello, %s!", $name);
+}
